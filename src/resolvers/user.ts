3a91cb38d56e9987fa483878b1fa7e8148c71bb1
gruf <gruf@codedrop.cc>
2021-12-22 16:06:24 +0600

    login resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import { MyContext } from '../types';
 import { User } from '../entities/User';
 import * as argon2 from 'argon2';
@@ -9,6 +9,22 @@ class UsernamePasswordInput {
   username: string;
   @Field()
   password: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], {nullable: true})
+  errors?: FieldError[];
+  @Field(() => User, {nullable: true})
+  user?: User;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
 }
 
 @Resolver()
@@ -26,5 +42,38 @@ export class UserResolver {
     await em.persistAndFlush(user);
 
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() {em}: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {username: options.username});
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: `Couldn't fiend such a user with login ${options.username}`
+          },
+        ]
+      };
+    }
+    const isPasswordValid = await argon2.verify(user.password, options.password);
+    if (!isPasswordValid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Wrong password'
+          }
+        ]
+      };
+    }
+
+    return {
+      user
+    };
   }
 }
