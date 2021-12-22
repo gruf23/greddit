5c118b02b31aa41cc431279c953adfd5c7405455
gruf <gruf@codedrop.cc>
2021-12-22 20:56:23 +0600

    user session with cookies and me resolver

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,59 @@ import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { __prod__ } from './constants';
+import { MyContext } from './types';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+
+declare module "express-session" {
+  interface Session {
+    userId: number;
+  }
+}
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
   const app = express();
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  const sessionMiddleware = session({
+    name: 'qid',
+    store: new RedisStore({
+      client: redisClient, //
+      disableTouch: true,
+      disableTTL: true,
+    }),
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: __prod__ // https only
+    },
+    saveUninitialized: false,
+    secret: 'rendomstringyo',
+    resave: false,
+  });
+
+  app.use(sessionMiddleware);
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
-    context: () => ({
-      em: orm.em
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(), // need this to get old playground which is okay w/ cookies
+    ],
+    context: ({req, res}): MyContext => ({
+      em: orm.em,
+      req,
+      res
     })
   });
 
