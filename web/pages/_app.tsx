69b6c49478e4d42226655b5e71e29717fe28bc03
gruf <gruf@codedrop.cc>
2021-12-24 18:39:53 +0600

    graphcache, user fragment

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,12 +1,61 @@
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { cacheExchange, QueryInput, Cache } from '@urql/exchange-graphcache';
 import '../styles/globals.css';
+import { MeDocument } from '../generated/graphql';
+
+function betterUpdateQuery<Result, Query> (
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result as Result, data as any) as any)
+}
 
 const urqlClient = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: 'include'
-  }
-})
+  },
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (_result, args, cache, info) => {
+          betterUpdateQuery(
+            cache,
+            {query: MeDocument},
+            _result,
+            (result: any, query) => {
+              if (result.login.errors) {
+                return query
+              } else {
+                return {
+                  me: result.login.user,
+                }
+              }
+            }
+            );
+        },
+        register: (_result, args, cache, info) => {
+          betterUpdateQuery(
+            cache,
+            {query: MeDocument},
+            _result,
+            (result: any, query) => {
+              if (result.register.errors) {
+                return query
+              } else {
+                return {
+                  me: result.register.user,
+                }
+              }
+            }
+          );
+        }
+      }
+    }
+  }), fetchExchange]
+});
 
 function Greddit({Component, pageProps}) {
   return (
